c795fc8a7bd9da22dda2984a66f9e933710d04e3
SulaimaanS <Sulaimaansharif@y7mail.com>
2021-04-20 10:01:47 +1000

    created SQL tables

--- web/iotbdb/tables.sql
+++ web/iotbdb/tables.sql
@@ -1,9 +1,21 @@
-CREATE TABLE PRODUCTTABLE(
-    productID int NOT NULL, 
-    "name" varchar(30) NOT NULL,
-    description varchar(100),
-    category varchar(30),
-    price float(10),
-    stock int,
-    PRIMARY KEY (productID)
+CREATE TABLE USERTABLE(
+    userID int NOT NULL, 
+    firstName varchar(30) NOT NULL,
+    lastName varchar(30) NOT NULL,
+    dob date NOT NULL,
+    phoneNumber varchar(20) NOT NULL,
+    emailAddress varchar(50) NOT NULL,
+    streetNumber int NOT NULL,
+    streetName varchar(20) NOT NULL,
+    postCode int NOT NULL,
+    PRIMARY KEY (userID)
+);
+
+
+CREATE TABLE STAFFTABLE(
+    userID int NOT NULL, 
+    staffID int NOT NULL,
+    authorised boolean,
+    PRIMARY KEY (staffID),
+    FOREIGN KEY (userID) REFERENCES USERTABLE(userID)
 );
