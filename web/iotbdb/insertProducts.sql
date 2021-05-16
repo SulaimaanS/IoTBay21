89189b7091d47fdfc3dec5b249a8df8e388456af
SulaimaanS <Sulaimaansharif@y7mail.com>
2021-05-17 01:54:02 +1000

    first 2 func done. Others incomplete or not integrated

--- web/iotbdb/insertProducts.sql
+++ web/iotbdb/insertProducts.sql
@@ -1,12 +1,22 @@
-INSERT INTO CUSTOMERTABLE(USERID, DOB, STREETNUMBER, STREETNAME, POSTCODE, REGISTERED)
+INSERT INTO PRODUCTTABLE(PRODUCTNAME, PRODUCTDESCRIPTION, PRODUCTCATEGORY, PRODUCTPRICE, PRODUCTSTOCK)
 VALUES
-(1,'01/28/2001',25,'Clara Street',2000,'True'),
-(2,'05/12/2001',10,'Sega Street',2000,'True'),
-(3,'06/21/2001',2,'Keys Street',2102,'True'),
-(4,'04/11/2001',20,'Prophet Street',2039,'True'),
-(5,'12/01/2001',99,'Whale Street',2020,'True'),
-(6,'10/06/2001',404,'Fx Street',2019,'True'),
-(7,'07/11/2001',101,'Sesame Street',2839,'True'),
-(8,'10/09/2001',573,'Konami Street',2019,'True'),
-(9,'04/11/2001',13,'Kyuu Street',1029,'True'),
-(10,'06/11/2001',21,'Satisfaction Street',1902,'True');
+('Outdoor Camera', 'something useful', 'surveillance', 550.99, 50),
+('Indoor Camera', 'something useful', 'surveillance', 550.99, 50),
+('Canon 50D', 'A nice camera', 'Cameras', 600.50, 50),
+('Satellite Dish', 'something Dish-y', 'Entertainment', 9050.43, 3),
+('Outdoor Camera', 'something useful', 'surveillance', 550.99, 50),
+('Indoor Camera', 'something useful', 'surveillance', 550.99, 50),
+('Canon 50D', 'A nice camera', 'Cameras', 600.50, 50),
+('Satellite Dish', 'something Dish-y', 'Entertainment', 9050.43, 3),
+('Outdoor Camera', 'something useful', 'surveillance', 550.99, 50),
+('Indoor Camera', 'something useful', 'surveillance', 550.99, 50),
+('Canon 50D', 'A nice camera', 'Cameras', 600.50, 50),
+('Satellite Dish', 'something Dish-y', 'Entertainment', 9050.43, 3),
+('Outdoor Camera', 'something useful', 'surveillance', 550.99, 50),
+('Indoor Camera', 'something useful', 'surveillance', 550.99, 50),
+('Canon 50D', 'A nice camera', 'Cameras', 600.50, 50),
+('Satellite Dish', 'something Dish-y', 'Entertainment', 9050.43, 3),
+('Outdoor Camera', 'something useful', 'surveillance', 550.99, 50),
+('Indoor Camera', 'something useful', 'surveillance', 550.99, 50),
+('Canon 50D', 'A nice camera', 'Cameras', 600.50, 50),
+('Satellite Dish', 'something Dish-y', 'Entertainment', 9050.43, 3);
